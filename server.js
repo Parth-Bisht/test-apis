22d7c19f3e0c5feebce7b5439dba99f0518eb220
Parth Bisht <112894309+bisht-parth@users.noreply.github.com>
2023-10-12 18:10:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data.json");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+console.log(data);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/meals/india", (req, res) => {
+  res.status(200).json({ data });
+});
+
+app.listen(4000, (req, res) => {
+  console.log(`Server running on port 4000`);
+});
